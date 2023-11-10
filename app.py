138f1b92e098622231ce9bcd3feb6d4cb38d673e
Reza Panahi <rezaanahiads2@gmail.com>
2023-11-10 07:15:10 +0330

    modify files

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-from Login import Login
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-Login()
+driver = webdriver.Firefox()
+
+driver.get('https://vc.farspnu.ac.ir/Student/HomeWork')
+
+def Login():
+    username = driver.find_element(By.ID, 'Input_UserName')
+    password = driver.find_element(By.ID, 'password')
+    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    
+    username.send_keys('401956553')
+    password.send_keys('2420946596aA')
+    login_button.click()
